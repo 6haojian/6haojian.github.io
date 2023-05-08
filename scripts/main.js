e817b64478281504fbaa2e2161392472a92719cf
6haojian <hjan_lau@hotmail.com>
2023-05-08 21:23:49 +0800

    v0.1.0

--- scripts/main.js
+++ scripts/main.js
@@ -1,35 +1,10 @@
-let myImage = document.querySelector('img');
-myImage.onclick = function() {
-    let mySrc = myImage.getAttribute('src');
-    if (mySrc === 'images/yinyang2.jpeg') {
-        myImage.setAttribute('src', 'images/yinyang.jpeg');
+const nav = document.querySelector('.nav')
+window.addEventListener('scroll', fixNav)
+
+function fixNav() {
+    if(window.scrollY > nav.offsetHeight + 150) {
+        nav.classList.add('active')
     } else {
-        myImage.setAttribute('src', 'images/yinyang2.jpeg');
+        nav.classList.remove('active')
     }
-}
-
-let myButton = document.querySelector('button');
-let myHeading = document.querySelector('h1');
-
-function setUserName() {
-    let myName = prompt('请输入你的名字：');
-    if(!myName) {
-        setUserName();
-    } else {
-        localStorage.setItem('name', myName);
-        myHeading.textContent = 'Hello, ' + myName;
-    }
-    
-    
-}
-
-if(!localStorage.getItem('name')) {
-    setUserName();
-} else {
-    let storedName = localStorage.getItem('name');
-    myHeading.textContent = 'Hello, ' + storedName;
-}
-
-myButton.onclick = function() {
-    setUserName();
 }
